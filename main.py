4b71964d3b5e79c5973dfac65907c0a3dbbfa693
Kriepp <satmarclaudiu83@gmail.com>
2025-01-10 22:00:34 +0200

    Done

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 def main():
     book_path = "books/frankenstein.txt"
+    print(f"--- Begin report of {book_path} ---")
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    # dict_to_list = list(count_char(text))
-    # dict_sorted = dict_to_list.sort(reverse=True, key=sort_on)
     print(f"{num_words} words found in the document")
-    # print_report = report(dict_sorted)
-    count_char(num_words)
+    char_count = count_char(text)
+    report(char_count)
+    print("--- End report ---")
+
 
 
 def get_num_words(text):
@@ -20,23 +21,26 @@ def get_book_text(path):
 
 def count_char(text):
     lower_text = text.lower()
-    count = []
+    count = {}
     for c in lower_text:
         if c in count:
-            count["c"]['name'] += 1
+            count[c] += 1
         else:
-            count["c"]['name'] = 1
-    print(f"{count}")
+            count[c] = 1
     return count
+
+def report(sorted_dict):
+    list_of_dict = []
+    for char, count in sorted_dict.items():  
+        if char.isalpha():
+            new_dict = {"name": char, "num": count}
+            list_of_dict.append(new_dict)
+    list_of_dict.sort(reverse=True, key=sort_on)
+
+    for item in list_of_dict:
+        print(f"The '{item['name']}' character was found {item['num']} times")
 
 def sort_on(dict):
     return dict["num"]
-
-#! Create a dictionary of dictionaries
-def report(sorted_dict):
-    for c in sorted_dict:
-        if c.isalpha():
-            print(f"{c} Is alpha")   
-            
 
 main()
